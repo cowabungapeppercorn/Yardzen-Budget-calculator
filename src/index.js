2660a620f4b532affbfcab55ad4d796d4c1670db
Nick <nst447@gmail.com>
2020-02-23 14:17:41 -0800

    Installed react-bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import seeds from './seeds';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 window.seed = seeds;
 
