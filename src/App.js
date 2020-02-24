8ccf68c24fe29bc8d6f8bdedbc0145583d2dc3f9
Nick <nst447@gmail.com>
2020-02-24 11:36:53 -0800

    App renders components correctly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React  from 'react';
+import React, { useContext } from 'react';
 import './App.css';
+import BudgetForm from './components/BudgetForm';
+import BudgetChecklist from './components/BudgetChecklist';
+import { Context as BudgetContext } from './context/BudgetContext';
+import BudgetAnalysis from './components/BudgetAnalysis';
+
 
 function App() {
+  const { state: { amount, submitted } } = useContext(BudgetContext);
+
   return (
-    <div className="App">
-      {/* good luck */}
+    <div className="App container">
+      <h1 className="my-5">Yardzen Budget Calculator</h1>
+
+      {!amount && (
+        <BudgetForm />
+      )}
+
+      {amount && !submitted && (
+        <BudgetChecklist  />
+      )}
+
+      {submitted && (
+        <BudgetAnalysis />
+      )}
+
     </div>
   );
 }
