fda2b88e67edfe318334df0d3b510416b99ee5b9
Nick <nst447@gmail.com>
2020-02-24 11:54:19 -0800

    Changed alert messages slightly

--- src/helpers/returnBudgetMessage.js
+++ src/helpers/returnBudgetMessage.js
@@ -1,8 +1,8 @@
 export default (amount, low, high) => {
   if (amount >= high) {
-    return ["You are under budget at the highest costs.", "success"]
+    return ["You are under budget at the highest estimated costs.", "success"]
   } else if (amount <= low) {
-    return ["You are over budget at the lowest costs.", "danger"]
+    return ["You are over budget at the lowest estimated costs.", "danger"]
   } else {
     return ["You are within the estimated budget.", "success"]
   }
