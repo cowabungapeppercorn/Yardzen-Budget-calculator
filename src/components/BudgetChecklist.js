37830ebd0462779c603312b4227f364a8b12b9a1
Nick <nst447@gmail.com>
2020-02-23 17:44:37 -0800

    Checklist components functional, waiting on context creation

--- src/components/BudgetChecklist.js
+++ src/components/BudgetChecklist.js
@@ -1,32 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { getAllItems } from '../api';
+import React from 'react';
 import ChecklistGroup from './ChecklistGroup';
+import useGetDbItems from '../hooks/useGetDbItems';
+import sortItemsByType from '../helpers/sortItemsByType';
+import Form from 'react-bootstrap/Form';
 
 export default function BudgetChecklist({ setSelectedItems }) {
-  const [items, setItems] = useState({});
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function _getAllItems() {
-      const response = await getAllItems();
-      setItems(response);
-      setLoading(false);
-    };
-    _getAllItems();
-  }, []);
-
+  const [items, loading] = useGetDbItems();
+  
   if (loading) {
     return <h1>LOADING...</h1>
   }
+  
+  const itemsObj = sortItemsByType(items);
 
   return (
     <div style={{ border: '5px solid black' }}>
       <h3>Budget Checklist Component</h3>
-      <div>
-        {Object.keys(items).map((type, i) => (
-          <ChecklistGroup key={i} items={items[type]} title={type} setSelectedItems={setSelectedItems} />
-        ))}
-      </div>
+      <Form>
+        {Object.keys(itemsObj).map((type, i) => {
+          return <ChecklistGroup key={i} items={itemsObj[type]} title={type} setSelectedItems={setSelectedItems} />
+        })}
+      </Form>
     </div>
-  )
-}
+  );
+};
--- src/components/ChecklistGroup.js
+++ src/components/ChecklistGroup.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import camelToTitleCase from '../helpers/camelToTitleCase';
 import Form from 'react-bootstrap/Form';
 
 export default function ChecklistGroup({ items, title, setSelectedItems }) {
+  const [selected, setSelected] = useState('');
+
   return (
     <div style={{ marginTop: '20px', border: '5px solid green' }}>
       <h3>{camelToTitleCase(title)}</h3>
-      <Form>
         <Form.Group>
-          {items.map(item => (
-            <Form.Check
+          {items.map(item => {
+            return <Form.Check
               type="checkbox"
               label={`${item.name}: $${item.lowPrice} - $${item.highPrice}`}
               key={item.id}
-              checked={true}
+              checked={item.id === selected}
+              onChange={item.id !== selected 
+                ? () => setSelected(item.id)
+                : () => setSelected('')
+              }
             />
-          ))}
+          })}
         </Form.Group>
-      </Form>
     </div>
-  )
-}
+  );
+};
