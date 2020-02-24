dcee26da31c40fa7d8a7abd9d2d29479c17255ab
Nick <nst447@gmail.com>
2020-02-24 11:37:41 -0800

    Last unsaved changes

--- src/hooks/useGetDbItems.js
+++ src/hooks/useGetDbItems.js
@@ -1,15 +1,20 @@
-import { useState, useEffect } from 'react';
-import { getAllItems } from '../api';
+import { useState, useEffect, useContext } from 'react';
+import { getAllItems } from '../api/api';
+import { Context as BudgetContext } from '../context/BudgetContext';
+import sortItemsByType from '../helpers/sortItemsByType';
 
 export default () => {
   const [items, setItems] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { setTypes } = useContext(BudgetContext);
 
   useEffect(() => {
     async function _getAllItems() {
-      const response = await getAllItems();
-      setItems(response);
+      const itemsArr = await getAllItems();
+      const itemsObj = sortItemsByType(itemsArr);
+      setItems(itemsObj);
       setLoading(false);
+      setTypes(Object.keys(itemsObj));
     };
     _getAllItems();
   }, []);
--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import seeds from './seeds';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider as BudgetProvider } from './context/BudgetContext';
+
 
 window.seed = seeds;
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BudgetProvider><App /></BudgetProvider>, document.getElementById('root'));
 
 serviceWorker.unregister();
