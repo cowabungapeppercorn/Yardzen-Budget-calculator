c3a6b7ebb3852bdf5c29c193caa71212180060b5
Nick <nst447@gmail.com>
2020-02-24 11:35:47 -0800

    Moved api file to its own folder

---
+++ src/api/api.js
@@ -0,0 +1,20 @@
+import db from '../db';
+
+const getAllItems = async () => {
+  try {
+    const itemsArr = [];
+    const response = await db.collection("items").get();
+    response.forEach(doc => {
+      const item = doc.data();
+      item.id = doc.id;
+      itemsArr.push(item);
+    });
+
+    return itemsArr;
+  
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { getAllItems };
